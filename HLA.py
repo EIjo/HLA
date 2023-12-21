d0297c1a2f174b15fc143a06c9f3e8385f45f058
Eljo Dorrestijn <eljo@live.nl>
2023-12-21 14:16:35 +0100

    added functionality to cluster histogram based on amino acid properties: class and polarity

--- HLA.py
+++ HLA.py
@@ -3,7 +3,7 @@ import pandas
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
-from HLA_util import find_amino_acids, find_all_amino_acids_fast
+from HLA_util import find_amino_acids, find_all_amino_acids_fast, cluster_counter
 
 parser = argparse.ArgumentParser(
     description=''' 
@@ -23,6 +23,11 @@ parser.add_argument(
     help='if set the tool will output the total amount of amino-acids found in file '
          'as well as an histogram of different amino-acids found per position'
 )
+parser.add_argument(
+    '--cluster', '-cl', nargs=1, required=False,
+    help='if set the amino-acid histogram will be clustered together based on a shared property. '
+         'Currently class and polarity are the options available'
+)
 
 
 if __name__ == '__main__':
@@ -32,19 +37,28 @@ if __name__ == '__main__':
     if args.position:
         amino_acids = find_amino_acids(file, int(args.position[0]))
         amino_acids_counts = Counter(amino_acids)
-        df = pandas.DataFrame.from_dict(amino_acids_counts, orient='index')
-        df.plot(kind='bar')
-        plt.show()
+
+        if args.cluster:
+            amino_acids_counts_cluster = cluster_counter(amino_acids_counts, args.cluster[0])
+            df = pandas.DataFrame.from_dict(amino_acids_counts_cluster, orient='index')
+            df.plot(kind='bar')
+            plt.show()
+        else:
+            df = pandas.DataFrame.from_dict(amino_acids_counts, orient='index')
+            df.plot(kind='bar')
+            plt.show()
 
     if args.count:
         amino_acids_counter = find_all_amino_acids_fast(file)
+
         total_amino_acids = Counter()
         variations = []
         for c in amino_acids_counter:
             total_amino_acids.update(c)
             common_variations = 0
             for aa, count in c.items():
-                if count > 96:
+                # common variation needs to be higher than 1% of all records
+                if count > int(amino_acids_counter[0].total()/100):
                     common_variations += 1
             variations.append(common_variations)
 
