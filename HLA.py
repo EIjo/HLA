0fe6863217de97ee5dee9c6554f94d03d92a6fd7
Eljo Dorrestijn <eljo@live.nl>
2023-12-27 16:00:36 +0100

    added extra function to choose a threshold value that minimizes duplicate ids and maximizes completeness of all ids

--- HLA.py
+++ HLA.py
@@ -33,6 +33,11 @@ parser.add_argument(
     help='if set changes the threshold of minimum amount of proteins needed '
          'before they are considered expressed by the spesific amino acid at the specified location'
 )
+parser.add_argument(
+    '--threshold_test', '-tt', action='store_true',
+    help='if set a plot will be generated showing the amount of protein_ids includes for each set threshold up to 200,'
+         'as well as the amount of duplicate ids shared between amino acids'
+)
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -42,13 +47,42 @@ if __name__ == '__main__':
         amino_acids, amino_acids_ids = HLA_util.find_amino_acids(file, int(args.position[0]))
         amino_acids_counts = Counter(amino_acids)
 
+        # sort by amino acid counts (by highest count first)
+        amino_acids_counts = dict(sorted(amino_acids_counts.items(), key=lambda x: -x[1]))
+        amino_acids_ids = HLA_util.sort_by_other_dict(amino_acids_ids, amino_acids_counts)
+
         if args.protein_threshold:
             HLA_util.get_unique_proteins(amino_acids_ids, int(args.protein_threshold[0]))
         else:
             HLA_util.get_unique_proteins(amino_acids_ids)
 
+        if args.threshold_test:
+            t = np.arange(0, 200, 1)
+            completeness_list, duplicates_list = HLA_util.calc_threshold_proteins(amino_acids_ids)
+            fig, ax1 = plt.subplots()
+
+            color = 'tab:red'
+            ax1.set_xlabel('threshold value')
+            ax1.set_ylabel('%completeness', color=color)
+            ax1.plot(t, completeness_list, color=color)
+            ax1.tick_params(axis='y', labelcolor=color)
+
+            ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+            color = 'tab:blue'
+            ax2.set_ylabel('#duplicates', color=color)  # we already handled the x-label with ax1
+            ax2.plot(t, duplicates_list, color=color)
+            ax2.tick_params(axis='y', labelcolor=color)
+
+            fig.tight_layout()  # otherwise the right y-label is slightly clipped
+            plt.show()
+
         if args.cluster:
             amino_acids_counts_cluster = HLA_util.cluster_counter(amino_acids_counts, args.cluster[0])
+
+            # sort by amino acid cluster counts (by highest count first)
+            amino_acids_counts = dict(sorted(amino_acids_counts.items(), key=lambda x: -x[1]))
+
             df = pandas.DataFrame.from_dict(amino_acids_counts_cluster, orient='index')
             df.plot(kind='bar')
             plt.show()
