cc72e6132528e1a7ba856cfaf3bdcb6643f6ffee
Eljo Dorrestijn <eljo@live.nl>
2023-12-27 17:16:48 +0100

    improved data output and plots

--- HLA.py
+++ HLA.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
 import HLA_util
+import json
 
 parser = argparse.ArgumentParser(
     description=''' 
@@ -43,6 +44,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
     file = args.input[0]
 
+    with open('Annotations/amino_acid_annotations.json') as fd:
+        aa_annotations = json.load(fd)
+
     if args.position:
         amino_acids, amino_acids_ids = HLA_util.find_amino_acids(file, int(args.position[0]))
         amino_acids_counts = Counter(amino_acids)
@@ -52,9 +56,17 @@ if __name__ == '__main__':
         amino_acids_ids = HLA_util.sort_by_other_dict(amino_acids_ids, amino_acids_counts)
 
         if args.protein_threshold:
-            HLA_util.get_unique_proteins(amino_acids_ids, int(args.protein_threshold[0]))
+            aa_ids_threshold = HLA_util.get_unique_proteins(amino_acids_ids, int(args.protein_threshold[0]))
         else:
-            HLA_util.get_unique_proteins(amino_acids_ids)
+            aa_ids_threshold = HLA_util.get_unique_proteins(amino_acids_ids)
+
+        for aa, prot_ids in aa_ids_threshold.items():
+            output = ''
+            for prot_id in prot_ids:
+                output += prot_id + ', '
+            if len(output) > 0:
+                print(f'{aa} ({aa_annotations[aa]["name"]})')
+                print(output[:-2])
 
         if args.threshold_test:
             t = np.arange(0, 200, 1)
@@ -67,29 +79,31 @@ if __name__ == '__main__':
             ax1.plot(t, completeness_list, color=color)
             ax1.tick_params(axis='y', labelcolor=color)
 
-            ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+            ax2 = ax1.twinx()
 
             color = 'tab:blue'
-            ax2.set_ylabel('#duplicates', color=color)  # we already handled the x-label with ax1
+            ax2.set_ylabel('#duplicates', color=color)
             ax2.plot(t, duplicates_list, color=color)
             ax2.tick_params(axis='y', labelcolor=color)
 
-            fig.tight_layout()  # otherwise the right y-label is slightly clipped
+            fig.tight_layout()
             plt.show()
 
         if args.cluster:
-            amino_acids_counts_cluster = HLA_util.cluster_counter(amino_acids_counts, args.cluster[0])
+            amino_acids_counts_cluster = HLA_util.cluster_counter(amino_acids_counts, args.cluster[0], aa_annotations)
 
             # sort by amino acid cluster counts (by highest count first)
-            amino_acids_counts = dict(sorted(amino_acids_counts.items(), key=lambda x: -x[1]))
+            amino_acids_counts = dict(sorted(amino_acids_counts_cluster.items(), key=lambda x: -x[1]))
 
-            df = pandas.DataFrame.from_dict(amino_acids_counts_cluster, orient='index')
-            df.plot(kind='bar')
-            plt.show()
-        else:
-            df = pandas.DataFrame.from_dict(amino_acids_counts, orient='index')
-            df.plot(kind='bar')
-            plt.show()
+        fig = plt.figure(figsize=(10, 5))
+
+        # creating the bar plot
+        plt.bar(list(amino_acids_counts.keys()), list(amino_acids_counts.values()))
+
+        plt.xlabel("Amino acids")
+        plt.ylabel("Amino acid frequency")
+        plt.title("Amino acid frequency at position 116")
+        plt.show()
 
     if args.count:
         amino_acids_counter = HLA_util.find_all_amino_acids_fast(file)
@@ -103,12 +117,21 @@ if __name__ == '__main__':
                 # common variation needs to be higher than 1% of all records
                 if count > int(amino_acids_counter[0].total()/100):
                     common_variations += 1
+            if common_variations >= 6:
+                print(len(variations)-23)
             variations.append(common_variations)
 
         # actual position different from position in variations due to negative start position
         variations = np.array(variations)
-        print('max variations = ' + str(np.max(variations)))
-        print('average variations = ' + str(np.average(variations)))
 
+        print(f'max variations = {str(np.max(variations))}')
+        print(f'average variations = {str(np.average(variations))}')
 
+        variations_counts = Counter(variations)
+        plt.bar(list(variations_counts.keys()), list(variations_counts.values()))
+
+        plt.xlabel("Number of variations")
+        plt.ylabel("Frequency of variations")
+        plt.title("Frequency of number of variations of amino acids per position")
+        plt.show()
 
